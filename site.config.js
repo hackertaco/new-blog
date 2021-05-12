7e331cacbfc0c656311d14a1c0dbc7ed739cc85f
hackertaco <imsa1224@naver.com>
2021-05-12 16:57:48 +0900

    first

--- site.config.js
+++ site.config.js
@@ -7,9 +7,9 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'taco',
+  domain: 'taco.it',
+  author: 'taco',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,8 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'hackertaco',
+  linkedin: 'imsa1224',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
