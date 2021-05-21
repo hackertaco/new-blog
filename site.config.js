dc8320f6035e550974955c1c8a32acf1514973eb
hackertaco <imsa1224@naver.com>
2021-05-21 14:39:27 +0900

    fix: 🔥domain change

--- site.config.js
+++ site.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
   // basic site info (required)
   name: 'taco',
-  domain: 'taco.it',
+  domain: 'https://new-blog-brown.vercel.app/',
   author: 'taco',
 
   // open graph metadata (optional)
